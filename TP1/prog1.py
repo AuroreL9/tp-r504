4acfd3772a8b79bc1fac36f8ea6e5bb6384d16f4
Aurore <auroreleglaner@gmail.com>
2025-09-02 02:27:35 -1100

    modif

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+limport fonctions as f
+
+while True:
+	a=int(input("Donner un nombre :"))
+	b=int(input("Donner un nombre :"))
+
+	print("a puissance b est égal à ",f.puissance(a,b))
