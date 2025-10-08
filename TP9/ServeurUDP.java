c7d0f6ce89dd4c9f3f6e1745c7e2f0845154453e
AuroreL9 <auroreleglaner@gmail.com>
2025-10-08 15:04:18 +0200

    fin TP5 signaux POSIX et debut TP9

---
+++ TP9/ServeurUDP.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientUDP{
+	public static void main (String[] args )
+	{
+		DatagramSocket sock = new DatagramSocket(1234);
+		while(true)
+		{
+			System.out.println( "-Waiting data" );
+			DatagramPacket packet = new DatagramPacket(new bytes[1024], 1024);
+			sock.receive(packet);
+			String str = new String(packet.getData() );
+			System.out.println( "str=" + str );
+		}
+	}
+}
+
