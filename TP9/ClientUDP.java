8c2df56c906bf272f36299907387659e46d1645d
AuroreL9 <auroreleglaner@gmail.com>
2025-10-08 15:40:30 +0200

    fin partie 2

--- TP9/ClientUDP.java
+++ TP9/ClientUDP.java
@@ -2,16 +2,31 @@ import java.io.*;
 import java.net.*;
 
 public class ClientUDP{
-	public static void main (String[] args )
-	{
-		InetAddress addr = InetAddress.getLocalHost();
-		System.out.println( "adesse=" +addr.getHostName() );
+	public static void main (String[] args ) {
+		try {
+			String s = "Hello World";
+			byte[] data = s.getBytes();
 
-		byte[] data = s.getBytes();
+			InetAddress addr = InetAddress.getLocalHost();
+			System.out.println( "adesse=" +addr.getHostName() );
 
-		DatagramPacket packet = new DatagramPacket( data, data.length, addr, 1234 );
-		DatagramSocket sock = new DatagramSocket();
-		sock.send(packet);
-		sock.close();
+			DatagramPacket packet = new DatagramPacket( data, data.length, addr, 1234 );
+
+			DatagramSocket sock = new DatagramSocket();
+			sock.send(packet);
+			System.out.println("Message envoyés !");
+
+			byte[] buffer = new byte[1024];
+			DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+			sock.receive(response);
+			String reply = new String(response.getData(), 0, response.getLength());
+			System.out.println("Réponse reçue du serveur : " + reply);
+
+			sock.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
+
--- TP9/ServeurUDP.java
+++ TP9/ServeurUDP.java
@@ -1,18 +1,27 @@
 import java.io.*;
 import java.net.*;
 
-public class ClientUDP{
-	public static void main (String[] args )
-	{
-		DatagramSocket sock = new DatagramSocket(1234);
-		while(true)
-		{
-			System.out.println( "-Waiting data" );
-			DatagramPacket packet = new DatagramPacket(new bytes[1024], 1024);
-			sock.receive(packet);
-			String str = new String(packet.getData() );
-			System.out.println( "str=" + str );
+public class ServeurUDP{
+	public static void main (String[] args ) {
+		try {
+			DatagramSocket sock = new DatagramSocket(1234);
+			while(true)
+			{
+				System.out.println("- Waiting data...");
+				byte[] buffer = new byte[1024];
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				sock.receive(packet);
+				String str = new String(packet.getData(), 0, packet.getLength());
+				System.out.println("Message reçu : " + str);
+				InetAddress clientAddr = packet.getAddress();
+				int clientPort = packet.getPort();
+				DatagramPacket response = new DatagramPacket(
+				str.getBytes(), str.getBytes().length, clientAddr, clientPort);
+				sock.send(response);
+				System.out.println("Message renvoyé au client.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
-
